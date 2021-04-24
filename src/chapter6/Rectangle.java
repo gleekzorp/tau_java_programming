5e69d5d849dac36893a160d035b2f78a8abe2d32
Daniel <gleek.dan@gmail.com>
2021-04-24 15:43:48 -0600

    Add Chapter 6a - Classes and Objects

---
+++ src/chapter6/Rectangle.java
@@ -0,0 +1,44 @@
+package chapter6;
+
+public class Rectangle {
+
+    private double length;
+    private double width;
+
+    public Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
+    public Rectangle(double length, double width){
+        // there is two ways of doing this
+        // the traditional way I'm used to from JS
+        // or use your setter method
+        this.length = length;
+        setWidth(width);
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public void setLength(double length){
+        this.length = length;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+
+    public double calculatePerimeter(){
+        return (2 * length) + (2 * width);
+    }
+
+    public double calculateArea(){
+        return length * width;
+    }
+}
