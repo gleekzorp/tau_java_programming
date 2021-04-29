66b8e9175898f97ac363aa68f87cc89bbb717fcb
Daniel <gleek.dan@gmail.com>
2021-04-28 22:08:19 -0600

    Add Chapter 9c - Overriding and Overloading Methods

--- src/chapter9/InheritanceTester.java
+++ src/chapter9/InheritanceTester.java
@@ -10,5 +10,19 @@ public class InheritanceTester {
 
         Square square = new Square();
         square.print("square");
+
+        testSquareOverride();
+    }
+
+    public static void testSquareOverride(){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(4);
+        rectangle.setWidth(8);
+        System.out.println(rectangle.calculatePerimeter());
+
+        Square square = new Square();
+        square.setLength(4);
+        square.setWidth(8);
+        System.out.println(square.calculatePerimeter());
     }
 }
